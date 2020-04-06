93554bcc863f930bd36350260fe604b169925d8e
Powell Wheeler <powell.wheeler@gmail.com>
2020-04-06 11:12:08 -0400

    removing some datasets

--- data-raw/d9SamplingPoints.R
+++
@@ -1,2 +0,0 @@
-d9.sampling.points <- read.csv('~/Dropbox/NCWRC/GPS/d9.sampling.points.csv', na = '.', header=T)
-save(d9.sampling.points,file='~/Dropbox/R/packages/wheelR/data/d9SamplingPoints.rda')
