32aa9cbcb7afb3d21d6992c3c9ca51a819d30f27
Powell Wheeler <powell.wheeler@gmail.com>
2020-04-06 11:07:37 -0400

    removing some datasets

---
+++ data-raw/coord.range.48.states.R
@@ -0,0 +1,2 @@
+coord.range.48.states <- read.csv('~/Dropbox/NCWRC/GPS/coord.range.of.48.states.csv', row.names =1, header=T)
+save(coord.range.48.states, file='~/git/wheelR/data/coord.range.48.states.rda')
